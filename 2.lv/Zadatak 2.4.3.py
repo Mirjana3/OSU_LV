eaeebb3c8e516e60d311b7bcbc734eccb0a9af8f
Mirjana3 <156024390+Mirjana3@users.noreply.github.com>
2025-05-05 18:33:42 +0200

    Update Zadatak 2.4.3.py

--- 2.lv/Zadatak 2.4.3.py
+++ 2.lv/Zadatak 2.4.3.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 
 img = plt.imread("road.jpg")
 
-brighter = np.clip(img*1.5, 0, 1)
+brighter = np.clip(img*1.5, 0, 1) # clip - ograničava vrijednost [0,1]
 
-h, w = img.shape[:2]
-quarter = img[:, w//4:w//2]
+h, w = img.shape[:2] # dimenzija slike, ali uzima se samo prva dva elementa visina i širina
+quarter = img[:, w//4:w//2] # uzimaju se svi redovi, dio slike od četvrtine širine (w//4) do polovice širine (w//2) - // cijeli broj dijeljenja
 
-rotated = np.rot90(img, -1)
+rotated = np.rot90(img, -1) # -1 obrnuto od kazaljske sata
 
-mirrored = np.fliplr(img)
+mirrored = np.fliplr(img) # izvrće sliku s lijeva na desno
 
 fig, axs = plt.subplots(2, 2, figsize=(10, 8))
 axs[0, 0].imshow(brighter, cmap='gray')
